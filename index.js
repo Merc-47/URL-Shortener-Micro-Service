937f7d9329859a79fc93328131a843b53ebf2c45
ThathsaraD <144412644+ThathsaraD@users.noreply.github.com>
2025-08-27 15:13:26 +0530

    url shortener

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -18,6 +19,36 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+let urlDatabase = [];
+
+
+//URL shortener endpoint
+app.post("/api/shorturl", (req, res) => {
+  const original_url = req.body.url;
+
+  const urlPattern = /^(http|https):\/\/.+$/;
+  if (!urlPattern.test(original_url)) {
+    return res.json({ error: "invalid url" });
+  }
+
+  const short_url = urlDatabase.length + 1;
+  urlDatabase.push({ original_url, short_url });
+
+  res.json({ original_url, short_url });
+});
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const short_url = parseInt(req.params.short_url);
+  const found = urlDatabase.find(u => u.short_url === short_url);
+
+  if (found) {
+    res.redirect(found.original_url);
+  } else {
+    res.json({ error: "invalid url" });
+  }
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
